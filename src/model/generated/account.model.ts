47b1519dffe1b5e7fa991b38bea239489a6601d1
Alexander Belopashentsev <abelopashentsev@gmail.com>
2022-07-30 16:18:31 +0300

    rework template

--- src/model/generated/account.model.ts
+++ src/model/generated/account.model.ts
@@ -1,6 +1,5 @@
 import {Entity as Entity_, Column as Column_, PrimaryColumn as PrimaryColumn_, OneToMany as OneToMany_} from "typeorm"
-import * as marshal from "./marshal"
-import {HistoricalBalance} from "./historicalBalance.model"
+import {Transfer} from "./transfer.model"
 
 @Entity_()
 export class Account {
@@ -14,9 +13,9 @@ export class Account {
   @PrimaryColumn_()
   id!: string
 
-  @Column_("numeric", {transformer: marshal.bigintTransformer, nullable: false})
-  balance!: bigint
+  @OneToMany_(() => Transfer, e => e.to)
+  transfersTo!: Transfer[]
 
-  @OneToMany_(() => HistoricalBalance, e => e.account)
-  historicalBalances!: HistoricalBalance[]
+  @OneToMany_(() => Transfer, e => e.from)
+  transfersFrom!: Transfer[]
 }
--- src/processor.ts
+++ src/processor.ts
@@ -3,7 +3,7 @@ import * as ss58 from "@subsquid/ss58"
 import {BatchContext, BatchProcessorItem, SubstrateBatchProcessor} from "@subsquid/substrate-processor"
 import {Store, TypeormDatabase} from "@subsquid/typeorm-store"
 import {In} from "typeorm"
-import {Account, HistoricalBalance} from "./model"
+import {Account, Transfer} from "./model"
 import {BalancesTransferEvent} from "./types/events"
 
 
@@ -11,13 +11,21 @@ const processor = new SubstrateBatchProcessor()
     .setBatchSize(500)
     .setDataSource({
         // Lookup archive by the network name in the Subsquid registry
-        archive: lookupArchive("kusama", { release: "FireSquid" })
+        archive: lookupArchive("kusama", {release: "FireSquid"})
 
         // Use archive created by archive/docker-compose.yml
         // archive: 'http://localhost:8888/graphql'
     })
     .addEvent('Balances.Transfer', {
-        data: {event: {args: true}}
+        data: {
+            event: {
+                args: true,
+                extrinsic: {
+                    hash: true,
+                    fee: true
+                }
+            }
+        }
     } as const)
 
 
@@ -26,10 +34,10 @@ type Ctx = BatchContext<Store, Item>
 
 
 processor.run(new TypeormDatabase(), async ctx => {
-    let transfers = getTransfers(ctx)
+    let transfersData = getTransfers(ctx)
 
     let accountIds = new Set<string>()
-    for (let t of transfers) {
+    for (let t of transfersData) {
         accountIds.add(t.from)
         accountIds.add(t.to)
     }
@@ -38,41 +46,40 @@ processor.run(new TypeormDatabase(), async ctx => {
         return new Map(accounts.map(a => [a.id, a]))
     })
 
-    let history: HistoricalBalance[] = []
+    let transfers: Transfer[] = []
 
-    for (let t of transfers) {
+    for (let t of transfersData) {
+        let {id, block, timestamp, extrinsic, amount, fee} = t
+
         let from = getAccount(accounts, t.from)
         let to = getAccount(accounts, t.to)
 
-        from.balance -= t.amount
-        to.balance += t.amount
-
-        history.push(new HistoricalBalance({
-            id: t.id + "-from",
-            account: from,
-            balance: from.balance,
-            timestamp: t.timestamp
-        }))
-
-        history.push(new HistoricalBalance({
-            id: t.id + "-to",
-            account: to,
-            balance: to.balance,
-            timestamp: t.timestamp
+        transfers.push(new Transfer({
+            id,
+            block,
+            timestamp,
+            extrinsic,
+            from,
+            to,
+            amount,
+            fee
         }))
     }
 
     await ctx.store.save(Array.from(accounts.values()))
-    await ctx.store.insert(history)
+    await ctx.store.insert(transfers)
 })
 
 
 interface TransferEvent {
     id: string
+    block: number
+    timestamp: Date
+    extrinsic?: string
     from: string
     to: string
     amount: bigint
-    timestamp: bigint
+    fee?: bigint
 }
 
 
@@ -84,7 +91,7 @@ function getTransfers(ctx: Ctx): TransferEvent[] {
                 let e = new BalancesTransferEvent(ctx, item.event)
                 let rec: {from: Uint8Array, to: Uint8Array, amount: bigint}
                 if (e.isV1020) {
-                    let [from, to, amount, ] = e.asV1020
+                    let [from, to, amount,] = e.asV1020
                     rec = {from, to, amount}
                 } else if (e.isV1050) {
                     let [from, to, amount] = e.asV1050
@@ -94,10 +101,13 @@ function getTransfers(ctx: Ctx): TransferEvent[] {
                 }
                 transfers.push({
                     id: item.event.id,
+                    block: block.header.height,
+                    timestamp: new Date(block.header.timestamp),
+                    extrinsic: item.event.extrinsic?.hash,
                     from: ss58.codec('kusama').encode(rec.from),
                     to: ss58.codec('kusama').encode(rec.to),
                     amount: rec.amount,
-                    timestamp: BigInt(block.header.timestamp)
+                    fee: item.event.extrinsic?.fee
                 })
             }
         }
@@ -111,7 +121,6 @@ function getAccount(m: Map<string, Account>, id: string): Account {
     if (acc == null) {
         acc = new Account()
         acc.id = id
-        acc.balance = 0n
         m.set(id, acc)
     }
     return acc
