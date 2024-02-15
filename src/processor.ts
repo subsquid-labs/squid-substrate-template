ebc1f9ae8e6f8e30ffa7ddae245647adf96843ee
abernatskiy <abernatskiy@toha.li>
2024-02-15 22:46:30 +0900

    New processor interface
    	modified:   src/processor.ts

--- src/processor.ts
+++ src/processor.ts
@@ -13,18 +13,16 @@ import {
 import {events} from './types'
 
 export const processor = new SubstrateBatchProcessor()
-    .setDataSource({
-        // Lookup archive by the network name in Subsquid registry
-        // See https://docs.subsquid.io/substrate-indexing/supported-networks/
-        archive: lookupArchive('kusama', {release: 'ArrowSquid'}),
-        // Chain RPC endpoint is required on Substrate for metadata and real-time updates
-        chain: {
-            // Set via .env for local runs or via secrets when deploying to Subsquid Cloud
-            // https://docs.subsquid.io/deploy-squid/env-variables/
-            url: assertNotNull(process.env.RPC_ENDPOINT),
-            // More RPC connection options at https://docs.subsquid.io/substrate-indexing/setup/general/#set-data-source
-            rateLimit: 10
-        }
+    // Lookup archive by the network name in Subsquid registry
+    // See https://docs.subsquid.io/substrate-indexing/supported-networks/
+    .setGateway(lookupArchive('kusama', {release: 'ArrowSquid'}))
+    // Chain RPC endpoint is required on Substrate for metadata and real-time updates
+    .setRpcEndpoint({
+        // Set via .env for local runs or via secrets when deploying to Subsquid Cloud
+        // https://docs.subsquid.io/deploy-squid/env-variables/
+        url: assertNotNull(process.env.RPC_ENDPOINT),
+        // More RPC connection options at https://docs.subsquid.io/substrate-indexing/setup/general/#set-data-source
+        rateLimit: 10
     })
     .addEvent({
         name: [events.balances.transfer.name],
